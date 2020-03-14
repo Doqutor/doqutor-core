e70dc3c20c8800b9fcf09ee0282df2a4b832b865
jatin7gupta <guptajatin@outlook.com>
2020-03-14 21:51:25 +1100

    Update infrastructure_stack.py
    added Iam policy to grant access to read and write data
    and table env name

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -7,12 +7,18 @@ class InfrastructureStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        # table create
         table = aws_dynamodb.Table(self, "doctors",
                                    partition_key=aws_dynamodb.Attribute(
                                        name="id",
                                        type=aws_dynamodb.AttributeType.STRING))
 
+        # Lambda create
         function = aws_lambda.Function(self, "backend",
                                        runtime=aws_lambda.Runtime.PYTHON_3_7,
                                        handler="handler.main",
                                        code=aws_lambda.Code.asset('./lambda'))
+
+        # IAM policy
+        table.grant_read_write_data(function)
+        function.add_environment("TABLE_NAME", table.table_name)
