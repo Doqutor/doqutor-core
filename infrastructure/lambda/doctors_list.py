9b5c298aa393dd056810b650590b25d78d8658b1
Mark Sonnenschein <marksonn5@gmail.com>
2020-03-15 21:33:32 +1100

    Fixed some CRUD response objects

--- infrastructure/lambda/doctors_list.py
+++ infrastructure/lambda/doctors_list.py
@@ -22,33 +22,6 @@ def doctor_list(id):
     return {
         'statusCode': 200,
         'headers': {'Content-Type': 'text/plain'},
-        'body': f'[Status: {response["ResponseMetadata"]["HTTPStatusCode"]}] Yeeting you over all doctors: {response["Items"]}'
+        'body': response["Items"]
     }
-
-
-#     import boto3
-# import logging
-# import json
-# import os
-# import uuid
-
-# LOG = logging.getLogger()
-# LOG.setLevel(logging.INFO)
-
-# def main(event, context):
-#     LOG.info("EVENT: " + json.dumps(event))
     
-#     return doctor_list(id)
-
-# def doctor_list(id):
-#     table_name = os.environ.get('TABLE_NAME')
-
-#     dynamodb = boto3.resource('dynamodb')
-#     table = dynamodb.Table(table_name)
-#     response = table.scan()
-
-#     return {
-#         'statusCode': 200,
-#         'headers': {'Content-Type': 'text/plain'},
-#         'body': response["Items"]
-#     }
