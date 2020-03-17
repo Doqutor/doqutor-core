75dd6fdded82772bd9e790655d6a50e9105f0e14
tom <hi@tomn.me>
2020-03-17 17:57:38 +1100

    allow multiple people to deploy to the same account

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -2,8 +2,13 @@
 
 from aws_cdk import core
 from infrastructure.infrastructure_stack import InfrastructureStack
+from config import get_config
+
+# fix for multiple people trying to deploy
+
+cfg = get_config()
 
 app = core.App()
-InfrastructureStack(app, "infrastructure", env={'region': 'ap-southeast-2'})
+InfrastructureStack(app, f"{cfg['prefix']}-infrastructure", env={'region': 'ap-southeast-2'})
 
 app.synth()
