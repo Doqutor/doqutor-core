60b0883736161f16dad706715e4574c81b1311d8
Judy Fan <dear.porcelain@gmail.com>
2020-04-19 18:12:01 +1000

    basic functionality of blocking done

--- js-infra/lambda/util/cloudtrail_ddb_access.py
+++ js-infra/lambda/util/cloudtrail_ddb_access.py
@@ -10,18 +10,8 @@ logger.setLevel(logging.INFO)
 client = boto3.client('iam')
 iam = boto3.resource('iam')
 
-def main(event, context):
-    #logger.info('Event details: %s', event['userIdentity'])
-    action = event['userIdentity']
-    username = action["userName"]
-    if action["type"] == "IAMUser":
-        user = iam.User(username)
-        group = iam.Group("adminusers")
-        admin_users = group.users.all()
-        ddb_iam_policy = {}
-        if user in admin_users:
-            print(username + " is admin")
-            ddb_iam_policy = {
+def make_ddb_policy():
+    ddb_iam_policy = {
                 "Version": "2012-10-17",
                 "Statement": [
                     {
@@ -48,31 +38,73 @@ def main(event, context):
                     }
                 ]
             }
-            
-            iam_deny_policy = {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                        "Sid": "VisualEditor0",
-                        "Effect": "Deny",
-                      "Action": "iam:*",
-                    "Resource": "*"
-                  }
-                ]
+
+    ddb_block = client.create_policy(
+        PolicyName='blockDynamoDBAccess',
+        PolicyDocument=json.dumps(ddb_iam_policy)
+    )
+    return ddb_block
+
+def make_iam_policy():
+    iam_deny_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "VisualEditor0",
+                "Effect": "Deny",
+                "Action": "iam:*",
+            "Resource": "*"
             }
+        ]
+    }
             
-            iam_block = client.create_policy(
-                PolicyName='blockIAMaccess',
-                PolicyDocument=json.dumps(iam_deny_policy)
-            )
+    iam_block = client.create_policy(
+        PolicyName='blockIAMaccess',
+        PolicyDocument=json.dumps(iam_deny_policy)
+    )
+    return iam_block
+    
+
+
+def main(event, context):
+    #logger.info('Event details: %s', event['userIdentity'])
+    action = event['userIdentity']
+    username = action["userName"]
+    if action["type"] == "IAMUser":
+        user = iam.User(username)
+        group = iam.Group("adminusers")
+        admin_users = group.users.all()
+        if user in admin_users:
+            print(username + " is admin")
+            policies = client.list_policies(Scope='Local')
+            #print(policies)
+            policy_list = policies['Policies']
+            print(policies['Policies'][2]['PolicyName'])
+            hasDbPolicy = False
+            hsaIAMPolicy = False
+            ddb_arn = ""
+            iam_arn = ""
+            for policy in policy_list:
+                if policy['PolicyName'] == 'blockDynamoDBAccess':
+                    hasDbPolicy = True
+                    ddb_arn = policy['Arn']
+                elif policy['PolicyName'] == 'blockIAMaccess':
+                    hasIAMPolicy = True
+                    iam_arn = policy['Arn']
+
             
-            response = client.create_policy(
-                PolicyName='blockDynamoDBAccess',
-                PolicyDocument=json.dumps(ddb_iam_policy)
-            )
-            print(response['Policy']['Arn'])
-            user.attach_policy(PolicyArn=response['Policy']['Arn'])
-            user.attach_policy(PolicyArn=iam_block['Policy']['Arn'])
+            if hasDbPolicy:
+                user.attach_policy(PolicyArn=ddb_arn)
+            else:
+                ddb_policy = make_ddb_policy()
+                user.attach_policy(PolicyArn=ddb_policy['Policy']['Arn'])
+
+
+            if hasIAMPolicy:
+                user.attach_policy(PolicyArn=iam_arn)
+            else:
+                iam_policy = make_iam_policy()
+                user.attach_policy(PolicyArn=iam_policy['Policy']['Arn'])
 
             
             #print(response)
