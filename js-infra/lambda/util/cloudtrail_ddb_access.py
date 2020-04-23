a64dc6b5fccd1636252df46f8cd4caa3c272c4cb
Judy Fan <dear.porcelain@gmail.com>
2020-04-23 22:50:28 +1000

    completed rollback on unauthorised insurance_id changes

--- js-infra/lambda/util/cloudtrail_ddb_access.py
+++ js-infra/lambda/util/cloudtrail_ddb_access.py
@@ -3,60 +3,38 @@ import logging
 import os
 import json
 
-TABLE_ARN = os.getenv('TABLE_ARN')
+TABLE_NAME = os.getenv('TABLE_NAME')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-client = boto3.client('iam')
-iam = boto3.resource('iam')
-
-# block all access to IAM
-def make_iam_policy():
-    iam_deny_policy = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "VisualEditor0",
-                "Effect": "Deny",
-                "Action": "iam:*",
-            "Resource": "*"
-            }
-        ]
-    }
-            
-    iam_block = client.create_policy(
-        PolicyName='blockIAMaccess',
-        PolicyDocument=json.dumps(iam_deny_policy)
-    )
-    return iam_block
-    
-
+dynamodb = boto3.resource('dynamodb')
 
 def main(event, context):
-    #logger.info('Event details: %s', event['userIdentity'])
     print(json.dumps(event))
-    action = event['detail']['userIdentity']
-    username = action["userName"]
-    if action["type"] == "IAMUser":
-        user = iam.User(username)
-        group = iam.Group("adminusers")
-        admin_users = group.users.all()
-        if user in admin_users:
-            print(username + " is admin")
-            policies = client.list_policies(Scope='Local')
-            policy_list = policies['Policies']
-            hasIAMPolicy = False
-            iam_arn = ""
-            for policy in policy_list:
-                if policy['PolicyName'] == 'blockIAMaccess':
-                    hasIAMPolicy = True
-                    iam_arn = policy['Arn']
-
-
-            if hasIAMPolicy:
-                user.attach_policy(PolicyArn=iam_arn)
-            else:
-                iam_policy = make_iam_policy()
-                user.attach_policy(PolicyArn=iam_policy['Policy']['Arn'])
+    if event['Records'][0]['eventName'] == 'MODIFY':
+        print('modify event')
+        curr = event['Records'][0]
+        # check on non alterable value
+        new_image = curr['dynamodb']['NewImage']['insurance_id']['S']
+        old_image = curr['dynamodb']['OldImage']['insurance_id']['S']
+        print("new: " + new_image + " old: " + old_image)
+        if new_image != old_image:
+            # trigger a "rollback" on bad value editing
+            print("rollback occurred")
+            print(curr['dynamodb']['Keys']['id']['S'])
+            table = dynamodb.Table(TABLE_NAME)
+            print(table.creation_date_time)
+            restore_item = {
+                'id': curr['dynamodb']['Keys']['id']['S'],
+                'name': curr['dynamodb']['OldImage']['name']['S'],
+                'email': curr['dynamodb']['OldImage']['email']['S'],
+                'phone_number': curr['dynamodb']['OldImage']['phone_number']['S'],
+                'age': curr['dynamodb']['OldImage']['age']['S'],
+                'is_active': curr['dynamodb']['OldImage']['is_active']['BOOL'],
+                'insurance_id': curr['dynamodb']['OldImage']['insurance_id']['S']
+            }
+            response = table.put_item(Item=restore_item)
+            print(response)
+    
     return event
      
