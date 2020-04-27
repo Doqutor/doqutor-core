ef3fd946913a5b14ade574e9569c8fdf21295af2
Judy Fan <dear.porcelain@gmail.com>
2020-04-27 20:19:31 +1000

    sns topic done. IR complete.

--- js-infra/lambda/util/cloudtrail_ddb_access.py
+++ js-infra/lambda/util/cloudtrail_ddb_access.py
@@ -4,10 +4,20 @@ import os
 import json
 
 TABLE_NAME = os.getenv('TABLE_NAME')
+SNS_ARN = os.getenv('SNS_ARN')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
+cloudtrail = boto3.client('cloudtrail')
+sns = boto3.client('sns')
+
+def publish_logging(client, sns_arn, subject, patient_id, modify_data):
+    client.publish(
+        TargetArn=sns_arn,
+        Subject=subject,
+        Message=f'Unauthorised modification of patient table data type {modify_data} on id: {patient_id}',
+    )
 
 def main(event, context):
     print(json.dumps(event))
@@ -15,26 +25,35 @@ def main(event, context):
         print('modify event')
         curr = event['Records'][0]
         # check on non alterable value
-        new_image = curr['dynamodb']['NewImage']['insurance_id']['S']
-        old_image = curr['dynamodb']['OldImage']['insurance_id']['S']
-        print("new: " + new_image + " old: " + old_image)
-        if new_image != old_image:
+        new_image = curr['dynamodb']['NewImage']
+        old_image = curr['dynamodb']['OldImage']
+        new_image_insurance_id = new_image['insurance_id']['S']
+        old_image_insurance_id = old_image['insurance_id']['S']
+        print("new: " + new_image_insurance_id + " old: " + old_image_insurance_id)
+        if new_image_insurance_id != old_image_insurance_id:
             # trigger a "rollback" on bad value editing
             print("rollback occurred")
             print(curr['dynamodb']['Keys']['id']['S'])
             table = dynamodb.Table(TABLE_NAME)
-            #print(table.creation_date_time)
+            target_id = curr['dynamodb']['Keys']['id']['S']
             restore_item = {
-                'id': curr['dynamodb']['Keys']['id']['S'],
-                'name': curr['dynamodb']['OldImage']['name']['S'],
-                'email': curr['dynamodb']['OldImage']['email']['S'],
-                'phone_number': curr['dynamodb']['OldImage']['phone_number']['S'],
-                'age': curr['dynamodb']['OldImage']['age']['S'],
-                'is_active': curr['dynamodb']['OldImage']['is_active']['BOOL'],
-                'insurance_id': curr['dynamodb']['OldImage']['insurance_id']['S']
+                'id': target_id,
+                'name': old_image['name']['S'],
+                'email': old_image['email']['S'],
+                'phone_number': old_image['phone_number']['S'],
+                'age': old_image['age']['S'],
+                'is_active': old_image['is_active']['BOOL'],
+                'insurance_id': old_image['insurance_id']['S']
             }
-            table.delete_item(Key={ 'id': curr['dynamodb']['Keys']['id']['S'] })
+            table.delete_item(Key={ 'id': target_id })
             response = table.put_item(Item=restore_item)
+            if response:
+                subject = "WARNING: Illegal write operation in Patient table"
+                publish_logging(client=sns, 
+                                sns_arn=SNS_ARN, 
+                                subject=subject, 
+                                modify_data="insurance_id",
+                                patient_id=target_id)
             print(response)
                 
     
