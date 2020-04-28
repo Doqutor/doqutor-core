e256d304c1b4d681dd152cfcecf9d7b60c67c1dc
Tom Nguyen <tom@tomn.me>
2020-04-28 16:09:28 +1000

    use cloudformation

--- js-infra/lib/monitoring-stack.ts
+++ js-infra/lib/monitoring-stack.ts
@@ -10,7 +10,7 @@ import * as iam from '@aws-cdk/aws-iam';
 import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 import * as cw_actions from '@aws-cdk/aws-cloudwatch-actions';
 import { Duration } from '@aws-cdk/core';
-//import * as wafv2 from '@aws-cdk/aws-wafv2';
+import * as wafv2 from '@aws-cdk/aws-wafv2';
 
 
 export class MonitoringStack extends cdk.Stack {
@@ -121,12 +121,6 @@ export class MonitoringStack extends cdk.Stack {
     ddbExcessReadAlarmDoc.addAlarmAction(new cw_actions.SnsAction(snsTopicCw));
     ddbExcessReadAlarmPat.addAlarmAction(new cw_actions.SnsAction(snsTopicCw));
 
-    // creating a lambda triggered by sns topic notification
-    //const debugLambda = createPythonLambda(this, 'util', 'dummy_lambda');
-    //snsTopicCw.addSubscription(new subscriptions.LambdaSubscription(debugLambda));
-
-
-    
     /*
     * IAM Policy to block admins from reading patient table
     */
@@ -155,5 +149,102 @@ export class MonitoringStack extends cdk.Stack {
       const ddbPatientBlockPolicy = iam.PolicyStatement.fromJson(ddbPatientBlock);
       policy.addStatements(ddbPatientBlockPolicy);
       policy.attachToGroup(adminGroup);
+
+    // WAF
+    // only allow 128 requests per 5 minutes, a pretty generous limit for a small practice
+    // assuming that a clinic is located at the same ip address
+    const wafAPI = new wafv2.CfnWebACL(this, 'waf-api', {
+      defaultAction: {
+        allow: {}
+      },
+      visibilityConfig: {
+        cloudWatchMetricsEnabled: true,
+        metricName: 'waf-apigateway',
+        sampledRequestsEnabled: true
+      },
+      scope: 'REGIONAL',
+      rules: [
+        {
+          name: "AWS-AWSManagedRulesAmazonIpReputationList",
+          priority: 0,
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: "AWS",
+              name: "AWSManagedRulesAmazonIpReputationList"
+            }
+          },
+          overrideAction: {
+            none: {}
+          },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "waf-apigateway-ipreputation"
+          }
+        },
+        {
+          name: "AWS-AWSManagedRulesCommonRuleSet",
+          priority: 1,
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: "AWS",
+              name: "AWSManagedRulesCommonRuleSet"
+            }
+          },
+          overrideAction: {
+            none: {}
+          },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "waf-apigateway-commonattacks"
+          }
+        },
+        {
+          name: 'ratelimit',
+          priority: 2,
+          statement: {
+            rateBasedStatement: {
+              limit: 128,
+              aggregateKeyType: 'IP'
+            }
+          },
+          visibilityConfig: {
+            cloudWatchMetricsEnabled: true,
+            metricName: 'waf-apigateway-ratelimit',
+            sampledRequestsEnabled: true
+          },
+          action: {
+            block: {}
+          }
+        }
+      ]
+    });
+    new wafv2.CfnWebACLAssociation(this, 'waf-assoc-apigateway', {
+      resourceArn: `arn:aws:apigateway:${this.region}::/restapis/${cdk.Fn.importValue(stackName+"-APIGateway")}/stages/prod`,
+      webAclArn: wafAPI.attrArn
+    });
+  
+    // grab cloudwatch metric and crate alarm
+    const wafMetric = new cloudwatch.Metric({
+      metricName: 'BlockedRequests',
+      namespace: 'AWS/WAFV2',
+      statistic: 'Sum',
+      dimensions: {
+        Rule: 'waf-apigateway-ratelimit',
+        WebACL: cdk.Fn.select(0, cdk.Fn.split('|', cdk.Fn.ref(wafAPI.logicalId))),
+        Region: 'ap-southeast-2'
+      }
+    });
+
+    const wafAlarm = new cloudwatch.Alarm(this, 'waf-apigateway-ratelimit-breached', {
+      metric: wafMetric,
+      threshold: 128,
+      period: Duration.seconds(60),
+      evaluationPeriods: 1,
+      datapointsToAlarm: 1,
+    });
+
+    wafAlarm.addAlarmAction(new cw_actions.SnsAction(snsTopicCw));
   }
 }
