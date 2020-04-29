7e1132428c901349afeac9577cab04d7aa64c13e
Tom Nguyen <tom@tomn.me>
2020-04-29 16:00:53 +1000

    add concurrency option

--- js-infra/lib/common/lambda.ts
+++ js-infra/lib/common/lambda.ts
@@ -2,21 +2,23 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 
 
-export function createPythonLambda(stack: cdk.Stack, folder: string, name: string): lambda.Function {
+export function createPythonLambda(stack: cdk.Stack, folder: string, name: string, concurrency?: number): lambda.Function {
     const fn = new lambda.Function(stack, name, {
         runtime: lambda.Runtime.PYTHON_3_7,
         handler: `${name}.main`,
-        code: new lambda.AssetCode(`./lambda/${folder}/`)
+        code: new lambda.AssetCode(`./lambda/${folder}/`),
+        reservedConcurrentExecutions: concurrency || undefined
     });
 
     return fn;
 }
 
-export function createTypeScriptLambda(stack: cdk.Stack, folder: string, name: string): lambda.Function {
+export function createTypeScriptLambda(stack: cdk.Stack, folder: string, name: string, concurrency?: number): lambda.Function {
     const fn = new lambda.Function(stack, name, {
         runtime: lambda.Runtime.NODEJS_12_X,
         handler: `${name}.handler`,
-        code: new lambda.AssetCode(`./lambda/${folder}`)
+        code: new lambda.AssetCode(`./lambda/${folder}`),
+        reservedConcurrentExecutions: concurrency || undefined
     });
 
     return fn;
