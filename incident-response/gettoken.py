6fb4305dc10fa0358ef605ac30caa4996315bc79
jmk11 <z5160822@student.unsw.edu.au>
2020-04-30 18:25:25 +1000

    gettoken update

--- incident-response/gettoken.py
+++ incident-response/gettoken.py
@@ -2,16 +2,47 @@ import sys
 import boto3
 import getpass
 from botocore.exceptions import ParamValidationError
+from typing import Callable
 
 
 cognito = boto3.client('cognito-idp')
 cloudformation = boto3.client('cloudformation')
 
-def getURL(stackname: str) -> str:
-    # build URL from stackname using cloudformation and cognito
-    # cognito.describe_user_pool()
-    #TODO
-    return "https://login-doqutore-infrastructure-prod.auth.ap-southeast-2.amazoncognito.com/login?client_id=1l26brptvhg0hhricpnno0h45d&response_type=token&scope=doqutore/application&redirect_uri=http://localhost"
+def searchStackResources(stackname: str, compareFn: Callable[[dict], bool]) -> str:
+    response: dict = cloudformation.list_stack_resources(
+            StackName = stackname
+    )
+    for item in response['StackResourceSummaries']:
+        if compareFn(item):
+            return item['PhysicalResourceId']
+    
+    nextToken = response.get('nextToken', None)
+    while nextToken != None:
+        response = cloudformation.list_stack_resources(
+            StackName = stackname,
+            NextToken = nextToken
+        )
+        for item in response['StackResourceSummaries']:
+            if compareFn(item):
+                return item['PhysicalResourceId']
+        nextToken = response.get('nextToken', None)
+    return None
+
+# return url, err
+def getURL(stackname: str, userpoolid: str) -> (str, str):
+    response = cognito.describe_user_pool(
+        UserPoolId = userpoolid
+    )
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        # do something
+        return None, 'describe user pool failed'
+
+    domain = response['UserPool']['Domain']
+    regionname = boto3.session.Session().region_name
+    compareFn = lambda item: item['LogicalResourceId'].startswith('appclient') and item['ResourceType'] == 'AWS::Cognito::UserPoolClient'
+    clientid = searchStackResources(stackname, compareFn)
+    url = 'https://' + domain + '.auth.' + regionname + '.amazoncognito.com' + '/login' + '?' + 'client_id=' + clientid + '&response_type=token&scope=doqutore/application&redirect_uri=http://localhost'
+    return url, None
 
 def extractToken(url: str) -> str:
     return "Bearer " + url.split("access_token=", 1)[1].split("&")[0]
@@ -29,7 +60,7 @@ def addUser(userpoolid: str, username: str, password: str) -> str:
                 },
                 {
                     'Name': 'email',
-                    'Value': 'a@aksdlfj.ajsi'
+                    'Value': 'testuser@jdjfjid.testuser'
                 },
                 {
                     'Name': 'custom:type',
@@ -66,51 +97,37 @@ def addUser(userpoolid: str, username: str, password: str) -> str:
         return err
     except cognito.exceptions.InvalidPasswordException as err:
         return err
+    except cognito.exceptions.InvalidParameterException as err:
+        return err
     # there must be another way to catch a series of exceptions
     # can't capture cognito.exceptions.* in variable name
     return None
 
 def getUserPoolid(stackname: str) -> str:
-    response: dict = cloudformation.list_stack_resources(
-        StackName = stackname
-    )
-    for item in response['StackResourceSummaries']:
-        if item['LogicalResourceId'].startswith('crmusers') and item['ResourceType'] == 'AWS::Cognito::UserPool':
-            return item['PhysicalResourceId']
-    '''for key in response:
-        if key == 'NextToken':
-            nextToken = response[key]'''
-    nextToken = response.get('nextToken', None)
-            
-    while nextToken != None: #response['NextToken'] != None:
-        response = cloudformation.list_stack_resources(
-            StackName = stackname,
-            NextToken = nextToken
-        )
-        for item in response['StackResourceSummaries']:
-            if item['LogicalResourceId'].startswith('crmusers') and item['ResourceType'] == 'AWS::Cognito::Userpool':
-                return item['PhysicalResourceId']
-        nextToken = response.get('nextToken', None)
+    compareFn = lambda item: item['LogicalResourceId'].startswith('crmusers') and item['ResourceType'] == 'AWS::Cognito::UserPool'
+    item = searchStackResources(stackname, compareFn)
+    return item
     # logical id crmusers44BE6CEC	
     # type AWS::Cognito::UserPool
-    return None
 
 def getToken(stackname: str) -> str:
+    userpoolid = getUserPoolid(stackname)
+    print(f'Userpoolid: {userpoolid}')
     newUserChoice = input('Create a new cognito user (Y) or use an existing user (N): ')
     if newUserChoice.upper() == 'Y':
         username = input('Username: ')
         password = getpass.getpass('Password: ')
         print('Creating new user...')
-        userpoolid = getUserPoolid(stackname)
-        print(f'Userpoolid: {userpoolid}')
         err = addUser(userpoolid, username, password)
         if err != None:
             print(err)
             return None
 
-    url = getURL(stackname)
-    print(f'Log in at this url and paste the redirect link here.\n{url}')
-    print()
+    url, err = getURL(stackname, userpoolid)
+    if err != None:
+        print(err)
+        return None
+    print(f'Log in at this url and paste the redirect link here.\n{url}\n')
     redirectlink = input()
     token = extractToken(redirectlink)
     return token
@@ -124,3 +141,4 @@ if __name__ == "__main__":
             print()
             print('Token:')
             print(token)
+            # need some way to make this easier to extract since can't use < or $() because there is other output
