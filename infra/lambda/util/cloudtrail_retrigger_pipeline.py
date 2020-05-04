18d2cefddd4403a0b68cfc01bef61401c8ca834e
Tom Nguyen <tom@tomn.me>
2020-05-04 20:41:25 +1000

    don't expose oauth keys in code

--- infra/lambda/util/cloudtrail_retrigger_pipeline.py
+++ infra/lambda/util/cloudtrail_retrigger_pipeline.py
@@ -7,6 +7,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 sns_client = boto3.client('sns')
+SNS_ARN = os.environ.get('SNS_ARN', '')
+GITHUB_KEY = os.environ.get('GITHUB_KEY', '')
 
 # Global variable hack is a temporary fix
 # Serverless functions should be stateless and idempotent
@@ -15,11 +17,11 @@ sns_client = boto3.client('sns')
 # It will just trigger the CI/CD pipeline multiple times which will queue them up on GitHub Actions
 already_run = False
 
-def publish_logging(client, sns_arn, event, subject, user):
-    client.publish(
-        TargetArn=sns_arn,
-        Subject=subject,
-        Message=f'CloudTrail event {event} invoked by user {user}',
+def publish_logging(event, user):
+    sns_client.publish(
+        TargetArn=SNS_ARN,
+        Subject='Unauthorised frontend modification detected',
+        Message=f'Unauthorised frontend deploy by user {user}. {event}',
     )
 
 def main(event, context):
@@ -31,15 +33,17 @@ def main(event, context):
 
     logger.info('Event details: %s', event['detail'])
 
-    sns_arn = os.environ['SNS_ARN']
+
 
     user = event['detail']['userIdentity']['userName']
     if user == 'frontend-deployment':
         return event
+
+    publish_logging(event=event, user=user)
     
     reqUrl = "https://api.github.com/repos/Doqutor/doqutor-frontend/dispatches"
     reqJson = {"event_type": "Redeploy from webhook!"}
-    reqAuth = ('aws-devops-bot', '047ffcdcb64eb943171429ea7fd34ebb9efc1ba1')
+    reqAuth = ('aws-devops-bot', GITHUB_KEY)
     reqHeaders = {"Accept":"application/vnd.github.everest-preview+json"}
     r = requests.post(reqUrl, json=reqJson, auth=reqAuth, headers=reqHeaders)
     return event
